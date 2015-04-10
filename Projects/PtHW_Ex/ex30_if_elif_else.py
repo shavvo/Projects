299fa2570c2eefd04234a6bbabdca296360b7500
aballens <aballens@rackspace.com>
2015-04-09 20:05:56 -0500

    Organizing and adding

---
+++ Projects/PtHW_Ex/ex30_if_elif_else.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We cant decide."
+
+
+if trucks > cars:
+    print "Thats too many trucks."
+elif trucks < cars:
+    print "Maybe we could take the trucks."
+else:
+    print "We still cant decide."
+
+if people > trucks:
+    print "Alright, lets just take the trucks."
+else:
+    print "Fine, lets stay home then."
