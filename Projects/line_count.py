89a8aa5a82d614120bbc05f2b8bbb3f74d0ab732
aballens <aballens@rackspace.com>
2015-04-14 20:24:39 -0500

    Line count function

---
+++ Projects/line_count.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def file_len(fname):
+    with open(fname) as txt:
+        for count, line in enumerate(txt):
+            pass
+    return count
+
+
+print file_len('/Users/aballens/system.log')
