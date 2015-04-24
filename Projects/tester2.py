f79571f37416e2a6f30b2e3654ddae870f4c6608
aballens <aballens@rackspace.com>
2015-04-23 21:07:26 -0500

    newest bad_block check

--- Projects/tester2.py
+++ Projects/tester2.py
@@ -3,7 +3,9 @@
 import os
 
 last_run_file = "/Users/aballens/last_run.txt"
+log_file = "/Users/aballens/system.log"
 
+# Open last run file and make sure it has something in it if empty.
 with open(last_run_file, 'a+') as last_run:
     last_run_read = last_run.readlines()
     size = last_run.tell()
@@ -13,3 +15,6 @@ with open(last_run_file, 'a+') as last_run:
     else:
         print "Its not empty, moving on"
 
+with open(log_file, 'r') as log:
+    log_read = log.readlines()
+    log_size = log.tell()`
