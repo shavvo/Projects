0795ea8ec3c7e6cf3ffe9bb0a0c544ac7055f3e0
Aaron Ballensky <aballens@rackspace.com>
2016-10-10 19:39:57 -0500

    Update sith_tool_v2.py
    sith tool rewrite

--- Projects/sith/sith_tool_v2.py
+++ Projects/sith/sith_tool_v2.py
@@ -2,54 +2,61 @@
 
 import json
 import sqlite3
-from datetime import timedelta
 from contextlib import contextmanager
 import sith
 
+
+data_table = '''
+CREATE TABLE IF NOT EXISTS prefail_drivedata
+(serial VARCHAR(40),
+ reallocated_event_cnt INTEGER,
+ reallocated_sect_cnt INTEGER,
+ current_pending_sect INTEGER,
+ offline_uncorrectable INTEGER,
+ CreateDate timestamp default (strftime('%s', 'now')));
+
+CREATE TABLE IF NOT EXISTS all_drivedata
+(serial VARCHAR(40),
+ attribute_name VARCHAR(40),
+ attribute_value VARCHAR(40),
+ CreateDate timestamp default (strftime('%s', 'now')));
+'''
+
 data = sith.Sith().process_smart_data()
-
-def create_columns():
-    columns = []
-    for key in data.values()[0].keys():
-        key = [i.replace("-", "_") for i in key]
-        key = "".join(key)
-        col = ",".join(["_%s TEXT" % key])
-        columns.append(col)
-    return columns
-
-def create_values():
-    values = []
-    for val in data.values():
-        values.append(val.values())
-    return values
-
-create_query = "CREATE TABLE IF NOT EXISTS drivedata({0});"
-create_query = create_query.format(", ".join(create_columns()))
 
 @contextmanager
 def smartdata_db():
-    conn = sqlite3.connect('/home/aballens/sith/smartdata2.db')
-    conn.execute(create_query)
+    conn = sqlite3.connect('/home/aballens/sith/smartdata3.db')
+    conn.executescript(data_table)
     conn.commit()
     yield conn
     conn.close()
 
 
 def insert_smartdata():
-    columns = []
-    for key in data.values()[0].keys():
-        key = [i.replace("-", "_") for i in key]
-        key = "".join(key)
-        col = ",".join(["_%s" % key])
-        columns.append(col)
+    with smartdata_db() as conn:
+        for key, val in data.iteritems():
+            for attr, rval in val.iteritems():
+                #insert all data into DB
+                conn.execute("insert into all_drivedata(serial, attribute_name, "
+                             "attribute_value) VALUES (?, ?, ?)",
+                             (key, attr, rval))
+                conn.commit()
 
-    for item in create_values():
-        item = ", ".join('"'+str(id)+'"' for id in item)
-        insert_query =  "insert into drivedata({0}) VALUES ({1})"
-        insert_query = insert_query.format(", ".join(columns), item)
-        with smartdata_db() as conn:
-            conn.execute(insert_query)
-            conn.commit()
+
+            if int(val['Reallocated_Event_Count']) != 0 or \
+                int(val['Reallocated_Event_Count']) != 0 or \
+                int(val['Current_Pending_Sector']) != 0 or \
+                int(val['Offline_Uncorrectable']) != 0:
+                    conn.execute("insert into prefail_drivedata(serial, reallocated_event_cnt, "
+                                 "reallocated_sect_cnt, current_pending_sect, offline_uncorrectable) "
+                                 "VALUES (?, ?, ?, ?, ?)",
+                                 (key, int(val['Reallocated_Event_Count']),
+                                  int(val['Reallocated_Event_Count']),
+                                  int(val['Current_Pending_Sector']),
+                                  int(val['Offline_Uncorrectable'])))
+                    conn.commit()
+
 
 if __name__ == '__main__':
     insert_smartdata()
