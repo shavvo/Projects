29f7bd4ce5a3d9ab51301a1a8d06d6c1514c336d
Aaron Ballensky <aballens@rackspace.com>
2016-11-01 20:28:40 -0500

    Update admintools_refac.py

--- Projects/random/admintools_refac.py
+++ Projects/random/admintools_refac.py
@@ -3,9 +3,12 @@
 import os
 import subprocess
 from prettytable import PrettyTable
+import sqlite3
 
 omreport = "/opt/dell/srvadmin/bin/omreport"
 omconfig = "/opt/dell/srvadmin/bin/omconfig"
+failures_db = '/etc/swift-admintools/db/failures.db'
+smartdata_db = '/etc/swift-admintools/db/smartdata.db'
 
 def _run(cmdline):
     p = subprocess.Popen(cmdline, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -173,3 +176,173 @@ def deletevdisk(controller, vdisk, **kwargs):
     rc, del_vdisk = _run('{0} storage vdisk action=deletevdisk '
                          'controller={1} vdisk={2}'.format(omconfig, controller, vdisk))
     print del_vdisk
+
+##########
+#Clear foreign controller config
+##########
+
+def clearforeignconfig(controller, **kwargs):
+    rc, clear_config = _run('{0} storage controller action=clearforeignconfig '
+                            'controller={1}'.format(omconfig, controller))
+    print clear_config
+
+##########
+#Import foreign controller config
+##########
+
+def importforeignconfig(controller, **kwargs):
+    rc, import_config = _run('{0} storage controller action=importrecoverforeignconfig '
+                             'controller={1}'.format(omconfig, controller))
+    print import_config
+
+##########
+#Discard preserved controller cache
+##########
+
+def discardpreservedcache(controller, **kwargs):
+    rc, disc_cache = _run('{0} storage controller action=discardpreservedcache '
+                          'controller={1} force=disabled'.format(omconfig, controller))
+    print disc_cache
+
+##########
+#Query admintools failure database
+##########
+
+def failures(**kwargs):
+    fields_tickets = ['ticket_number', 'created_date', 'failed_device', 'ticket_status']
+    fields_drives = ['created_date', 'failed_device', 'failed_port', 'failed_unit', 'serial',
+                     'model', 'capacity', 'progress']
+    ticket_headers = ['Ticket number', 'Creation time', 'Device', 'Status']
+    drive_headers = ['Creation time', 'Device', 'Port', 'Unit', 'Serial', 'Model', 'Capacity', 'Status']
+
+    if kwargs['full'] == True:
+        query_tickets = 'SELECT {0} FROM ticket_info'.format(','.join(fields_tickets))
+        query_drives = 'SELECT {0} FROM drive_info'.format(','.join(fields_drives))
+    else:
+        query_tickets = 'SELECT {0} FROM ticket_info WHERE ticket_status != 0'.format(','.join(fields_tickets))
+        query_drives = 'SELECT {0} FROM drive_info WHERE progress != 0'.format(','.join(fields_drives))
+
+
+    conn = sqlite3.connect(failures_db)
+    cur = conn.cursor()
+
+    #Tickets
+    print '>> Tickets'
+    res = cur.execute(query_tickets)
+    table = PrettyTable(ticket_headers)
+    table.align = 'r'
+    for row in res:
+        if row[-1] == 0:
+            status = "Completed"
+        elif row[-1] == 1:
+            status = "In Progress"
+        elif row[-1] == 2:
+            status = "Failed"
+        else:
+            status = "Unknown"
+        row = row[:-1] + (status,)
+        table.add_row(row)
+    print table
+
+    #Drives
+    print "\n>> Drives"
+    res = cur.execute(query_drives)
+    table = PrettyTable(drive_headers)
+    table.align = 'r'
+    for row in res:
+        if row[-1] == 0:
+            status = "Completed"
+        elif row[-1] == 1:
+            status = "In Progress"
+        elif row[-1] == 2:
+            status = "Failed"
+        else:
+            status = "Unknown"
+        row = row[:-1] + (status,)
+        table.add_row(row)
+    print table
+    conn.close()
+
+
+##########
+#Change status of drive to completed in failures DB
+##########
+
+def setcomplete(timestamp, **kwargs):
+    with sqlite3.connect(failures_db) as conn:
+        cur = conn.cursor()
+        try:
+            cur.execute('UPDATE ticket_info SET ticket_status = 0, pager = 0 WHERE created_date = ?',(timestamp,))
+            cur.execute('UPDATE drive_info SET progress = 0, pager = 0 WHERE created_date = ?',(timestamp,))
+            print "Success! {0} is set as completed".format(timestamp)
+        except sqlite3.OperationalError as e:
+            print "Database update failed: {0}".format(e)
+    conn.close()
+
+
+##########
+#Change status of drive to in progress in failures DB
+#########
+
+def setinprogress(timestamp, **kwargs):
+    with sqlite3.connect(failures_db) as conn:
+        cur = conn.cursor()
+        try:
+            cur.execute('UPDATE ticket_info SET ticket_status = 1 WHERE created_date = ?',(timestamp,))
+            cur.execute('UPDATE drive_info SET progress = 1 WHERE created_date = ?',(timestamp,))
+            print "Success! {0} is set as in progress".format(timestamp)
+        except sqlite3.OperationalError as e:
+            print "Database update failed: {0}".format(e)
+    conn.close()
+
+##########
+#Query smartdata db for drive smartdata values
+##########
+
+def smartdatareport(**kwargs):
+    with sqlite3.connect(smartdata_db) as conn:
+        cur = conn.cursor()
+        #Reports all drives smartdata
+        if kwargs['all'] == True:
+            cur.execute('SELECT * FROM all_drivedata')
+            col_name = [col[0] for col in cur.description]
+            all_rows = cur.fetchall()
+            output = PrettyTable(col_name)
+            for row in all_rows:
+                output.add_row(row)
+
+            output.set_field_align('attribute_name', 'l')
+            output.set_field_align('attribute_value', 'l')
+            print output
+
+        #Reports drives with bad smartdata values
+        if kwargs['report'] == True:
+            cur.execute('SELECT * FROM prefail_drivedata')
+            col_name = [col[0] for col in cur.description]
+            all_rows = cur.fetchall()
+            output = PrettyTable(col_name)
+            for row in all_rows:
+                output.add_row(row)
+
+            print output
+    conn.close()
+
+
+##########
+#Search smartdata DB for specific drive
+##########
+
+def smartdatasearch(serial, **kwargs):
+    with sqlite3.connect(smartdata_db) as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM all_drivedata where serial=?',(serial,))
+        col_name = [col[0] for col in cur.description]
+        all_rows = cur.fetchall()
+        output = PrettyTable(col_name)
+        for row in all_rows:
+            output.add_row(row)
+
+        output.set_field_align('attribute_name', 'l')
+        output.set_field_align('attribute_value', 'l')
+        print output
+    conn.close()
